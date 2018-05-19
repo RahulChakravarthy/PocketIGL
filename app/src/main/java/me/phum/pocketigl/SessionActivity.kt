fc9b66b205f639eb2ea9ab0e49598975b795095c
patzhum <patrick.hum@hotmail.com>
2018-05-19 17:09:11 -0400

    Connect sessionActivity to launcherActivity

--- app/src/main/java/me/phum/pocketigl/SessionActivity.kt
+++ app/src/main/java/me/phum/pocketigl/SessionActivity.kt
@@ -1,17 +1,21 @@
 package me.phum.pocketigl
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_session.*
 
 class SessionActivity : AppCompatActivity() {
-
+    private val firebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_session)
-
+        if (firebaseAuth.currentUser == null) {
+            Snackbar.make(root, "Not signed in!", Snackbar.LENGTH_INDEFINITE).show()
+        }
 
     }
 
