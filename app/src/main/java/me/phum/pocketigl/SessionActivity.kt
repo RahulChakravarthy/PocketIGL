03a1358ff098b4afb402609da50e87add6912a3a
edwinlo <e8lo@edu.uwaterloo.ca>
2018-05-19 19:37:24 -0400

    Added map activity and de_mirage as test map

--- app/src/main/java/me/phum/pocketigl/SessionActivity.kt
+++ app/src/main/java/me/phum/pocketigl/SessionActivity.kt
@@ -1,5 +1,6 @@
 package me.phum.pocketigl
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -10,12 +11,24 @@ import kotlinx.android.synthetic.main.activity_session.*
 
 class SessionActivity : AppCompatActivity() {
     private val firebaseAuth = FirebaseAuth.getInstance()
+    val context = this;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_session)
         if (firebaseAuth.currentUser == null) {
             Snackbar.make(root, "Not signed in!", Snackbar.LENGTH_INDEFINITE).show()
         }
+
+        //Uncomment the section below to navigate to the MapActivity using the 'Create New Section' button
+/*        val button1 = findViewById<Button>(R.id.createButton)
+
+        button1.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(view: View): Unit {
+                // Handler code here.
+                val intent = Intent(context, MapActivity::class.java);
+                startActivity(intent);
+            }
+        })*/
 
     }
 
