19dbc4334c611002444e4dd4283ecd50a9c6de05
Jay Weng <jay.weng7@gmail.com>
2018-05-19 19:23:45 -0400

    added create new session functionality

--- app/src/main/java/me/phum/pocketigl/SessionActivity.kt
+++ app/src/main/java/me/phum/pocketigl/SessionActivity.kt
@@ -7,6 +7,9 @@ import android.view.View
 import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_session.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 
 class SessionActivity : AppCompatActivity() {
     private val firebaseAuth = FirebaseAuth.getInstance()
@@ -17,6 +20,14 @@ class SessionActivity : AppCompatActivity() {
             Snackbar.make(root, "Not signed in!", Snackbar.LENGTH_INDEFINITE).show()
         }
 
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("pocketigl").child("sessions")
+
+        createButton.setOnClickListener {
+            val session = Session()
+            ref.child(session.sessionCode).setValue(session)
+            Snackbar.make(root, "Session code: " + session.sessionCode, Snackbar.LENGTH_LONG).show()
+        }
     }
 
 }
