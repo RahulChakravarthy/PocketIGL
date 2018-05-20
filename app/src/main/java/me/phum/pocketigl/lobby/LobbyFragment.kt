2982a39b817d589768a5f7dcac606d60a57bc946
cding <chris_ding78@yahoo.ca>
2018-05-19 21:55:25 -0400

    adding in userlist

--- app/src/main/java/me/phum/pocketigl/lobby/LobbyFragment.kt
+++ app/src/main/java/me/phum/pocketigl/lobby/LobbyFragment.kt
@@ -6,15 +6,14 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_lobby.*
-import me.phum.pocketigl.MapActivity
-
-import me.phum.pocketigl.R
-import me.phum.pocketigl.Session
-import me.phum.pocketigl.SessionActivity
+import me.phum.pocketigl.*
+import java.util.ArrayList
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,12 +29,21 @@ class LobbyFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private lateinit var sessionCode: String
     private var delegate : Delegate? = null
+    private val Users = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments!!.let {
             sessionCode = it.getString(ARG_SESSION_CODE)
         }
+
+    }
+
+    private fun initRecyclerView() {
+        val recyclerView = player_list
+        val adapter = RecyclerViewAdapter(Users, activity)
+        recyclerView.setAdapter(adapter)
+        recyclerView.setLayoutManager(LinearLayoutManager(activity))
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -54,6 +62,8 @@ class LobbyFragment : Fragment() {
         startButton.setOnClickListener {
             delegate?.onStartSession(sessionCode)
         }
+
+        initRecyclerView()
     }
 
     interface Delegate {
