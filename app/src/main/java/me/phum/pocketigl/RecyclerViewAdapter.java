9a7be96993a5a4682bd85f9c97cc72a6f36e9dfe
cding <chris_ding78@yahoo.ca>
2018-05-19 20:30:12 -0400

    ...

--- app/src/main/java/me/phum/pocketigl/RecyclerViewAdapter.java
+++ app/src/main/java/me/phum/pocketigl/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package me.phum.pocketigl;
 
 import android.content.Context;
+import android.content.Intent;
 import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -8,8 +9,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Gallery;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,25 +25,22 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         UserNames = userNames;
         mContext = context;
     }
-
-    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //Responsible for inflating view
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_user_list_item, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent,false);
         ViewHolder holder = new ViewHolder(view);
         return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Log.d(TAG,"onBindViewHolder: called. ");
-        holder.user.setText(UserNames.get(position));
-
-        holder.user.setOnClickListener(new View.OnClickListener(){
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        holder.USER.setText(UserNames.get(position));
+        holder.USER.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 //Patrick carries right here
+                Toast.makeText(mContext, UserNames.get(position), Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -51,11 +51,11 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView user;
+        TextView USER;
         RelativeLayout parentLayout;
         public ViewHolder(View itemView) {
             super(itemView);
-            user = itemView.findViewById(R.id.user);
+            USER = itemView.findViewById(R.id.user);
             parentLayout =itemView.findViewById(R.id.parent_layout);
         }
     }
