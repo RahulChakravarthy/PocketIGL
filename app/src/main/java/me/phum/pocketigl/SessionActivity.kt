086a0e55aeeb862411ac568beb0731ad71343480
Jay Weng <jay.weng7@gmail.com>
2018-05-19 20:51:39 -0400

    only join a session if it exists on the database

--- app/src/main/java/me/phum/pocketigl/SessionActivity.kt
+++ app/src/main/java/me/phum/pocketigl/SessionActivity.kt
@@ -6,8 +6,12 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.text.InputFilter
 import android.view.inputmethod.InputMethodManager
+import com.google.firebase.FirebaseError
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_session.*
 import me.phum.pocketigl.lobby.LobbyFragment
 
@@ -16,6 +20,7 @@ class SessionActivity : AppCompatActivity() {
     private val firebaseAuth = FirebaseAuth.getInstance()
     private var lobbyFragment : LobbyFragment? = null
     val context = this;
+    var currentSession = "";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_session)
@@ -23,9 +28,34 @@ class SessionActivity : AppCompatActivity() {
             Snackbar.make(root, "Not signed in!", Snackbar.LENGTH_INDEFINITE).show()
         }
 
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("pocketigl").child("sessions")
+
         joinButton.setOnClickListener {
-            joinSession(sessionCodeInput.text.toString() ?: "")
-            hideKeyboard()
+            if(sessionCodeInput.text.toString().length < 4) {
+                Snackbar.make(root, "Invalid session code!", Snackbar.LENGTH_SHORT).show()
+            } else if (currentSession == sessionCodeInput.text.toString()) {
+                Snackbar.make(root, "Already in this session!", Snackbar.LENGTH_LONG).show()
+            } else {
+                currentSession = sessionCodeInput.text.toString()
+
+                ref.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onDataChange(snapshot: DataSnapshot?) {
+                        val session = snapshot!!.child(currentSession)
+                        if(session.exists()) {
+                            Snackbar.make(root, "Joined session", Snackbar.LENGTH_SHORT).show()
+                            joinSession(currentSession)
+                        } else {
+                            Snackbar.make(root, "Session doesn't exist!", Snackbar.LENGTH_INDEFINITE).show()
+                        }
+                    }
+                })
+                hideKeyboard()
+            }
         }
 
         sessionCodeInput.filters = arrayOf(InputFilter.AllCaps(), InputFilter.LengthFilter(4))
@@ -41,17 +71,17 @@ class SessionActivity : AppCompatActivity() {
             }
         })*/
 
-        val database = FirebaseDatabase.getInstance()
-        val ref = database.getReference("pocketigl").child("sessions")
-
         createButton.setOnClickListener {
             val session = Session()
             ref.child(session.sessionCode).setValue(session)
             Snackbar.make(root, "Session code: " + session.sessionCode, Snackbar.LENGTH_LONG).show()
+            joinSession(session.sessionCode)
+            sessionCodeInput.setText(session.sessionCode)
         }
     }
 
     fun joinSession(sessionId: String) {
+        currentSession = sessionId
         val oldLobbyFragment = lobbyFragment
         lobbyFragment = LobbyFragment.newInstance(sessionId)
         supportFragmentManager.beginTransaction().apply {
